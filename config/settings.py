bacfd106758a372c617aca3768e36f6c1bcad5bb
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-05-20 11:54:40 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 JAZZMIN_SETTINGS = {
